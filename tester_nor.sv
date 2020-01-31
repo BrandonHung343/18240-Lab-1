aeee14267ebb63203289060658b017b69b938719
Stella Getz <sgetz@ece031.ece.local.cmu.edu>
2020-01-30 21:35:08 -0500

    test module redone

--- tester_nor.sv
+++ tester_nor.sv
@@ -21,22 +21,22 @@ module Tester (output logic a, b, c, d, e, f,
         {d, e, f} = 3'b111; //also def=111 test //because in 3 k-groups
 
     #10 {d, e, f} = 3'b010;
-    #10 {a, b, c} = 3'b011; // vowel=1
+        {a, b, c} = 3'b011; // vowel=1
 
     #10 {d, e, f} = 3'b100;
-    #10 {a, b, c} = 3'b100; //also abc=100 test //k-groups
+        {a, b, c} = 3'b100; //also abc=100 test //k-groups
     #10 {a, b, c} = 3'b101; //k-groups
 
     #10 {d, e, f} = 3'b111; 
-    #10 {a, b, c} = 3'b011; //k-groups
+        {a, b, c} = 3'b011; //k-groups
 
     #10 {d, e, f} = 3'b101;
-    #10 {a, b, c} = 3'b101;
+        {a, b, c} = 3'b101;
     #10 {a, b, c} = 3'b000;
 
     // valid=1, vowel=0
     #10 {a, b, c} = 3'b110;
-    #10 {d, e, f} = 3'b011;
+        {d, e, f} = 3'b011;
     #10 {a, b, c} = 3'b001;
     #10 {d, e, f} = 3'b110;
  
